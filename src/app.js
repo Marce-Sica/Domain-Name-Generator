c856b84a43d60d898cef3108b0b3d78abb1763f1
Marcelo Sica <marcelosica10@gmail.com>
2023-03-08 23:55:46 +0000

    Proyecto Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -8,4 +7,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("button").addEventListener("click", ejecutar);
+};
+
+let pronoun = ["conmigo", "nosotros", "ustedes"];
+let adj = ["chico", "alto", "rápido"];
+let noun = ["camisa", "teléfono", "silla"];
+let site = [".com", ".us", ".es", ".io"];
+const ejecutar = () => {
+  console.log("Click aqui");
+  let texto;
+  let parrafo;
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < site.length; l++) {
+          console.log(pronoun[i] + adj[j]);
+          parrafo = document.createElement("p");
+          texto = document.createTextNode(
+            pronoun[i] + adj[j] + noun[k] + site[l]
+          );
+          parrafo.appendChild(texto);
+          document.querySelector("#results-domains").appendChild(parrafo);
+        }
+      }
+    }
+  }
 };
